8c727f135a445caadefa3cf013f820fefa2fc7f0
shreyanshZevo <shreyansh.rathore@zevo360.com>
2023-09-19 17:15:21 +0530

    get request update for aws ec2 instance

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const authRouter = require('./routes/authRouter')
 app.use(cors())
 app.use(express.json());
 app.use('/', authRouter)
+app.get('/get', (req, res)=>{
+    res.send("Hello")
+})
 app.listen(8080, ()=>console.log("app is currently live on port 8080"))
